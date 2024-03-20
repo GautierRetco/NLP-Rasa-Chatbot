e537517673e9622c888755fda3d2c6768ba12116
Gautier <gautier.bigois@retco.app>
2024-03-20 16:47:32 +0100

    initial commit

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,32 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
+class ActionDisplayEntityHistory(Action):
+    def name(self) -> Text:
+        return "action_recommend_movie"
 
-# This is a simple example for a custom action which utters "Hello World!"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+        entity_history = {}
+
+        for event in reversed(tracker.events):
+            if 'parse_data' in event and 'entities' in event['parse_data']:
+                for entity in event['parse_data']['entities']:
+                    entity_name = entity['entity']
+                    entity_value = entity['value']
+                    if entity_name not in entity_history:
+                        entity_history[entity_name] = [entity_value]
+                    else:
+                        if entity_value not in entity_history[entity_name]:
+                            entity_history[entity_name].append(entity_value)
+
+        if entity_history:
+            for entity_name, values in entity_history.items():
+                dispatcher.utter_message(f"Historique des valeurs pour l'entité '{entity_name}': {', '.join(values)}")
+        else:
+            dispatcher.utter_message("Aucune entité n'a été détectée dans les messages précédents de la conversation.")
+
+        return []
